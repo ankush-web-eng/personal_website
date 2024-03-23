06925e44b264839e26eb3e5199ea2ba80a5e7658
Ankush <deshwalankush23@gmail.com>
2024-03-23 09:14:34 +0530

    video changed

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             <h1 className="text-5xl font-bold ">I make <span className="text-green-500">Full Stack</span> Web Applications</h1>
             <p className="text-gray-600 dark:text-gray-400"> Student, Writer, Content Creator, Developer, tech enthusiast with passion for <span className="text-blue-500">Code.</span></p>
             <div className="pt-8"><Sociallinks /></div>
-            <div className="pt-6"><video controls className="rounded-xl"><source src="/IMG_4564.MP4" /></video></div>
+            <div className="pt-6"><video controls className="rounded-xl"><source src="/" /></video></div>
             <div className="pt-8 pb-20 flex space-x-6 space-y-4 flex-wrap">
               <div className="w-fit px-4 py-4 dark:text-slate-800  flex flex-col border-2 border-gray-300 bg-teal-100 rounded-sm origin-top-left rotate-3 hover:rotate-0">Want to know more about me ?
               <span className="text-blue-500"><Link href="/about">Click here <FaArrowRight /></Link></span>
