e0aeeee1d4cf867d276604f677a9ebb03eee34cf
Ankush <deshwalankush23@gmail.com>
2024-06-08 17:26:59 +0530

    UI changed

--- components/ghost/ghost.tsx
+++ components/ghost/ghost.tsx
@@ -34,7 +34,7 @@ export default function Ghost({ params }: { params: form }) {
 
   return (
       <Link  href={`/kaizen/blogs/${params.id}`}
-        className="shadow-md border items-center justify-center relative rounded-xl text-white w-full h-52 flex flex-col space-y-3 p-2"
+        className="shadow-lg border items-center justify-center relative rounded-xl text-white w-full h-52 flex flex-col space-y-3 p-2"
         >
         <Image src={params.image} alt={params.title} width={500} height={230} style={{color: 'transparent'}} className="z-[-10] max-w-full max-h-full rounded-xl object-cover bg-repeat bg-over opacity-70 backdrop-filter backdrop-blur-2 absolute right-0 top-0" />
         <strong className="text-2xl font-bold drop-shadow-md">{params.title}</strong>
--- components/kaizen.tsx
+++ components/kaizen.tsx
@@ -70,7 +70,7 @@ export default function Kaizen() {
       </div>
 
 
-      <AllBlogs />
+      {/* <AllBlogs /> */}
       <AllGhosts />
 
 
