8d7ba9d03c18190e858a33f3c3f4325fcd5c6b82
Ankush <deshwalankush23@gmail.com>
2024-07-18 16:36:51 +0530

    refactor: Update social media links in Sociallinks component

--- components/includes/social.tsx
+++ components/includes/social.tsx
@@ -9,13 +9,13 @@ export default function Sociallinks() {
                 <Link aria-label="GitHub Link" target="_ankush" href="https://github.com/ankush-web-eng/"><FaGithub size={30} color="green" /></Link>
             </div>
             <div className="rounded-xl pl-10 pb-10 pt-2 pr-2 border bg-sky-200 border-sky-500 dark:bg-inherit">
-                <Link aria-label="LinkedIn Link" target="_ankush" href="https://www.linkedin.com/in/ankush-singh-b0389b27b/" ><FaLinkedin color="blue" size={30} /></Link>
+                <Link aria-label="LinkedIn Link" target="_ankush" href="https://www.linkedin.com/in/whyankush07" ><FaLinkedin color="blue" size={30} /></Link>
             </div>
             <div className="rounded-xl pl-10 pb-10 pt-2 pr-2 border bg-sky-200 border-sky-500 dark:bg-inherit">
-                <Link aria-label="Twitter Link" target="_ankush" href="https://twitter.com/Ankush__57s" ><FaTwitter color="blue" size={30} /></Link>
+                <Link aria-label="Twitter Link" target="_ankush" href="https://twitter.com/whyankush07" ><FaTwitter color="blue" size={30} /></Link>
             </div>
             <div className="rounded-xl pl-10 pb-10 pt-2 pr-2 border bg-sky-200 border-sky-500 dark:bg-inherit">
-                <Link aria-label="Instagram Link" target="_ankush" href="https://www.instagram.com/howankush07/" /><FaInstagram color="red" size={30} />
+                <Link aria-label="Instagram Link" target="_ankush" href="https://www.instagram.com/whyankush07/" /><FaInstagram color="red" size={30} />
             </div>
         </div>
     )
