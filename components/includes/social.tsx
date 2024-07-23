c333ca9d820ca61469bea763c32c9ad3afa425e8
Ankush <deshwalankush23@gmail.com>
2024-07-23 11:22:35 +0530

    Leetcode profile added and socila links svg updated

--- components/includes/social.tsx
+++ components/includes/social.tsx
@@ -1,22 +1,26 @@
 
+import Image from "next/image";
 import Link from "next/link"
 import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function Sociallinks() {
     return (
-        <div className="flex justify-evenly md:space-x-4 py-4 md:w-max w-screen">
-            <div className="rounded-xl pl-10 pb-10 pt-2 pr-2 border bg-sky-200 border-sky-500 dark:bg-inherit">
-                <Link aria-label="GitHub Link" target="_ankush" href="https://github.com/ankush-web-eng/"><FaGithub size={30} color="green" /></Link>
-            </div>
-            <div className="rounded-xl pl-10 pb-10 pt-2 pr-2 border bg-sky-200 border-sky-500 dark:bg-inherit">
-                <Link aria-label="LinkedIn Link" target="_ankush" href="https://www.linkedin.com/in/whyankush07" ><FaLinkedin color="blue" size={30} /></Link>
-            </div>
-            <div className="rounded-xl pl-10 pb-10 pt-2 pr-2 border bg-sky-200 border-sky-500 dark:bg-inherit">
-                <Link aria-label="Twitter Link" target="_ankush" href="https://twitter.com/whyankush07" ><FaTwitter color="blue" size={30} /></Link>
-            </div>
-            <div className="rounded-xl pl-10 pb-10 pt-2 pr-2 border bg-sky-200 border-sky-500 dark:bg-inherit">
-                <Link aria-label="Instagram Link" target="_ankush" href="https://www.instagram.com/whyankush07/" /><FaInstagram color="red" size={30} />
-            </div>
+        <div className="flex justify-evenly md:space-x-4 py-4 md:w-full w-screen">
+            <Link target="_blank" href={"https://github.com/ankush-web-eng/"}>
+                <Image src={'/github.svg'} height={36} width={36} alt="GitHub" />
+            </Link>
+            <Link target="_blank" href={"https://www.linkedin.com/in/whyankush07"}>
+                <Image src={'/linkedin.svg'} height={36} width={36} alt="LinkedIn" />
+            </Link>
+            <Link target="_blank" href={"https://twitter.com/whyankush07"}>
+                <Image src={'/twitter.svg'} height={36} width={36} alt="New" />
+            </Link>
+            <Link target="_blank" href={"https://leetcode.com/whyankush07"}>
+                <Image src={'/leetcode.svg'} height={36} width={36} alt="New" />
+            </Link>
+            <Link target="_blank" href={"https://www.instagram.com/whyankush07/"}>
+                <Image src={'/instagram.svg'} height={36} width={36} alt="Instagram" />
+            </Link>
         </div>
     )
 }
