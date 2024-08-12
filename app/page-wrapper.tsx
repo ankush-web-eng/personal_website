2ddc532f3834d697efd2fda3aafcd43d17bb05fa
Ankush <deshwalankush23@gmail.com>
2024-08-12 19:24:57 +0530

    UI updated

--- app/page-wrapper.tsx
+++ app/page-wrapper.tsx
@@ -1,23 +1,23 @@
 "use client"
 
-import {motion, AnimatePresence, useScroll} from "framer-motion"
+import { motion, AnimatePresence, useScroll } from "framer-motion"
 
-export default function PageWrapper({children}: {children: React.ReactNode}) {
+export default function PageWrapper({ children }: { children: React.ReactNode }) {
 
     const { scrollYProgress } = useScroll();
 
     return (
         <AnimatePresence>
-        <motion.div
-            // key={router.pathname}
-            initial={{opacity: 0, y:30}}
-            animate={{opacity: 1, y:0}}
-            exit={{opacity: 0, y:15}}
-            transition={{delay: 0.8, duration: 1.3, ease: 'easeInOut'}}
+            <motion.div
+                // key={router.pathname}
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 15 }}
+                transition={{ delay: 0.8, duration: 1.3, ease: 'easeInOut' }}
             // style={{ scaleX: scrollYProgress }}
-        >
-            {children}
-        </motion.div>
+            >
+                {children}
+            </motion.div>
         </AnimatePresence>
     )
 }
