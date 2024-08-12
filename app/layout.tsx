09e8c86942e3030bfc5036f1469bf402b89976b5
Ankush <deshwalankush23@gmail.com>
2024-08-12 20:33:25 +0530

    Meta data updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,31 +28,35 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta
-          name="description"
-          content="This is Ankush's professional space!"
-        />
-        <meta name="application-name" content="Ankush" />
+        <meta name="description" content="This is Ankush's professional space!" />
+        <meta name="application-name" content="Ankush's World" />
 
         <meta property="og:title" content="Ankush" />
-        <meta
-          property="og:description"
-          content="Welcome to Ankush's Professional Space!"
-        />
-        <meta property="og:image" content="/Ankush_pro.png" />
-        <meta property="og:image:alt" content="Ankush" />
+        <meta property="og:site_name" content="Ankush's World" />
+        <meta property="og:description" content="Welcome to Ankush's Professional Space!" />
+        <meta property="og:url" content="https://ankushsingh.tech" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image" content="https://ankushsingh.tech/Ankush_pro.png" />
+        <meta property="og:image:alt" content="Ankush's Professional Profile" />
 
         <meta name="twitter:title" content="Ankush" />
-        <meta
-          name="twitter:description"
-          content="Welcome to Ankush's Professional Space!"
-        />
-        <meta name="twitter:image" content="/anime.jpeg" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="Ankush__57s" />
+        <meta name="twitter:description" content="Welcome to Ankush's Professional Space!" />
 
-        <link rel="icon" type="image/png" href="/Ankush_pro.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <meta name="twitter:image" content="https://ankushsingh.tech/Ankush_pro.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="whyankush07" />
+
+        <meta name="linkedin:title" content="Ankush's World" />
+        <meta name="linkedin:description" content="Welcome to Ankush's Professional Space!" />
+        <meta name="linkedin:image" content="https://ankushsingh.tech/Ankush_pro.png" />
+
+        <meta property="instapp:hashtags" content="#personalbranding #professionalprofile" />
+        <meta property="instagram:author" content="@whyankush07" />
+
+        <link rel="icon" type="image/png" href="https://ankushsingh.tech/Ankush_pro.png" />
+        <link rel="manifest" href="https://ankushsingh.tech/manifest.json" />
         <meta name="theme-color" content="#33F9FF" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
--- app/provider.tsx
+++ app/provider.tsx
@@ -6,7 +6,7 @@ import { Navbar } from "@/components/navbar";
 export default function Provider({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <div className="flex max-md:flex-col overflow-y-auto overflow-x-hidden bg-gray-100">
+            <div className="flex max-md:flex-col overflow-y-auto overflow-x-hidden">
                 <Navbar />
                 <div className="flex h-screen w-full md:w-1/2 space-y-6 flex-col px-2 pt-8 md:pl-6">
                     <Header />
