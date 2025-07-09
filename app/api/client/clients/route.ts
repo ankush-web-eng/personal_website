771cb8155a6f7ccc2c9d7635847d1924f20e0bbc
Ankush <deshwalankush23@gmail.com>
2025-07-09 19:00:41 +0530

    minor-change

---
+++ app/api/client/clients/route.ts
@@ -0,0 +1,23 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    try {
+
+        const { searchParams } = new URL(req.url);
+        const secretCode = searchParams.get('secret_code');
+        if (!secretCode) {
+            return NextResponse.json({ success: false, message: 'Secret code is required' }, { status: 400 });
+        }
+
+        if (secretCode !== process.env.SECRET_CODE) {
+            return NextResponse.json({ success: false, message: 'Invalid secret code' }, { status: 403 });
+        }
+
+        const users = await prisma.clients.findMany();
+
+        return NextResponse.json({ success: true, data: users }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ success: false, message: 'An error occurred while fetching clients.' }, { status: 500 });
+    }
+}
